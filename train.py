0a0f3e3faf8c2a779396fb2af901d86fc2e87415
Arsh Zahed <arshzahed@gmail.com>
2024-09-17 23:17:09 -0700

    Add baseline profiling

--- train.py
+++ train.py
@@ -32,6 +32,7 @@ from models import DiT_models
 from diffusion import create_diffusion
 from diffusers.models import AutoencoderKL
 
+from torch.profiler import profile, record_function, ProfilerActivity
 
 #################################################################################
 #                             Training Helper Functions                         #
@@ -120,6 +121,7 @@ class CustomDataset(Dataset):
 #                                  Training Loop                                #
 #################################################################################
 
+
 def main(args):
     """
     Trains a new DiT model.
@@ -148,6 +150,7 @@ def main(args):
         input_size=latent_size,
         num_classes=args.num_classes
     )
+    
     # Note that parameter initialization is done within the DiT constructor
     model = model.to(device)
     ema = deepcopy(model).to(device)  # Create an EMA of the model for use after training
@@ -180,7 +183,7 @@ def main(args):
     model.train()  # important! This enables embedding dropout for classifier-free guidance
     ema.eval()  # EMA model should always be in eval mode
     model, opt, loader = accelerator.prepare(model, opt, loader)
-
+    
     # Variables for monitoring/logging purposes:
     train_steps = 0
     log_steps = 0
@@ -189,54 +192,67 @@ def main(args):
     
     if accelerator.is_main_process:
         logger.info(f"Training for {args.epochs} epochs...")
-    for epoch in range(args.epochs):
-        if accelerator.is_main_process:
-            logger.info(f"Beginning epoch {epoch}...")
-        for x, y in loader:
-            x = x.to(device)
-            y = y.to(device)
-            x = x.squeeze(dim=1)
-            y = y.squeeze(dim=1)
-            t = torch.randint(0, diffusion.num_timesteps, (x.shape[0],), device=device)
-            model_kwargs = dict(y=y)
-            loss_dict = diffusion.training_losses(model, x, t, model_kwargs)
-            loss = loss_dict["loss"].mean()
-            opt.zero_grad()
-            accelerator.backward(loss)
-            opt.step()
-            update_ema(ema, model)
 
-            # Log loss values:
-            running_loss += loss.item()
-            log_steps += 1
-            train_steps += 1
-            if train_steps % args.log_every == 0:
-                # Measure training speed:
-                torch.cuda.synchronize()
-                end_time = time()
-                steps_per_sec = log_steps / (end_time - start_time)
-                # Reduce loss history over all processes:
-                avg_loss = torch.tensor(running_loss / log_steps, device=device)
-                avg_loss = avg_loss.item() / accelerator.num_processes
-                if accelerator.is_main_process:
-                    logger.info(f"(step={train_steps:07d}) Train Loss: {avg_loss:.4f}, Train Steps/Sec: {steps_per_sec:.2f}")
-                # Reset monitoring variables:
-                running_loss = 0
-                log_steps = 0
-                start_time = time()
+    with torch.profiler.profile(
+        activities=[
+            torch.profiler.ProfilerActivity.CPU,
+            torch.profiler.ProfilerActivity.CUDA
+        ],
+        on_trace_ready=torch.profiler.tensorboard_trace_handler(f"{experiment_dir}/profiler-trace"),
+        record_shapes=True,
+        profile_memory=True,
+        with_stack=True
+    ) as p:
+        for epoch in range(args.epochs):
+            if accelerator.is_main_process:
+                logger.info(f"Beginning epoch {epoch}...")
+            for x,y in loader:
+                x = x.to(device)
+                y = y.to(device)
+                x = x.squeeze(dim=1)
+                y = y.squeeze(dim=1)
+                t = torch.randint(0, diffusion.num_timesteps, (x.shape[0],), device=device)
+                model_kwargs = dict(y=y)
+                loss_dict = diffusion.training_losses(model, x, t, model_kwargs)
+                loss = loss_dict["loss"].mean()
+                opt.zero_grad()
+                accelerator.backward(loss)
+                opt.step()
+                update_ema(ema, model)
 
-            # Save DiT checkpoint:
-            if train_steps % args.ckpt_every == 0 and train_steps > 0:
-                if accelerator.is_main_process:
-                    checkpoint = {
-                        "model": model.module.state_dict(),
-                        "ema": ema.state_dict(),
-                        "opt": opt.state_dict(),
-                        "args": args
-                    }
-                    checkpoint_path = f"{checkpoint_dir}/{train_steps:07d}.pt"
-                    torch.save(checkpoint, checkpoint_path)
-                    logger.info(f"Saved checkpoint to {checkpoint_path}")
+                # Log loss values:
+                running_loss += loss.item()
+                log_steps += 1
+                train_steps += 1
+                p.step()
+
+                if train_steps % args.log_every == 0:
+                    # Measure training speed:
+                    torch.cuda.synchronize()
+                    end_time = time()
+                    steps_per_sec = log_steps / (end_time - start_time)
+                    # Reduce loss history over all processes:
+                    avg_loss = torch.tensor(running_loss / log_steps, device=device)
+                    avg_loss = avg_loss.item() / accelerator.num_processes
+                    if accelerator.is_main_process:
+                        logger.info(f"(step={train_steps:07d}) Train Loss: {avg_loss:.4f}, Train Steps/Sec: {steps_per_sec:.2f}")
+                    # Reset monitoring variables:
+                    running_loss = 0
+                    log_steps = 0
+                    start_time = time()
+
+                # Save DiT checkpoint:
+                if train_steps % args.ckpt_every == 0 and train_steps > 0:
+                    if accelerator.is_main_process:
+                        checkpoint = {
+                            "model": model.module.state_dict(),
+                            "ema": ema.state_dict(),
+                            "opt": opt.state_dict(),
+                            "args": args
+                        }
+                        checkpoint_path = f"{checkpoint_dir}/{train_steps:07d}.pt"
+                        torch.save(checkpoint, checkpoint_path)
+                        logger.info(f"Saved checkpoint to {checkpoint_path}")
 
     model.eval()  # important! This disables randomized embedding dropout
     # do any sampling/FID calculation/etc. with ema (or model) in eval mode ...
